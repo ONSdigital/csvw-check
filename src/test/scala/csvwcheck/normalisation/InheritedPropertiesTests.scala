ed23a58dad144c29f630fa283f28e59f593325b3
Rob Barry <rob.barry@ons.gov.uk>
2023-06-12 16:38:32 +0100

    WIP - Making my way through the unit tests which have been commented out - bringing them back into use.

---
+++ src/test/scala/csvwcheck/normalisation/InheritedPropertiesTests.scala
@@ -0,0 +1,100 @@
+package csvwcheck.normalisation
+
+import NormalisationTestUtils.assertObjectNormalisation
+import org.scalatest.funsuite.AnyFunSuite
+import NormalisationTestUtils.MetadataWarningsExtensions
+
+
+class InheritedPropertiesTests extends AnyFunSuite {
+  test("null property returns value in array without warnings on valid value") {
+    val warnings = assertObjectNormalisation(
+      InheritedProperties.normalisers,
+      """
+        {
+          "null": "x"
+        }
+      """,
+      """
+        {
+          "null": ["x"]
+        }
+      """
+    )
+
+    assert(warnings.isEmpty, warnings)
+  }
+
+  test("null property returns warnings for invalid value (non string type)") {
+    val warnings = assertObjectNormalisation(
+      InheritedProperties.normalisers,
+      """
+        {
+          "null": false
+        }
+      """,
+      """
+        {
+          "null": [""]
+        }
+      """
+    )
+
+    assert(warnings.containsWarningWith("invalid_value"))
+  }
+
+  test("null property with values array holding different types") {
+    val warnings = assertObjectNormalisation(
+      InheritedProperties.normalisers,
+      """
+        {
+          "null": ["sample", 5]
+        }
+      """,
+      """
+        {
+          "null": ["sample"]
+        }
+      """
+    )
+
+    assert(warnings.containsWarningWith("invalid_value"))
+  }
+
+  test(
+    "separator property returns invalid warning if not of type string or null"
+  ) {
+    val warnings = assertObjectNormalisation(
+      InheritedProperties.normalisers,
+      """
+        {
+          "separator": false
+        }
+      """,
+      """
+        {
+          "separator": ""
+        }
+      """
+    )
+
+    assert(warnings.containsWarningWith("invalid_value"))  }
+
+  test("separator property returns value on valid case") {
+    val warnings = assertObjectNormalisation(
+      InheritedProperties.normalisers,
+      """
+        {
+          "separator": ","
+        }
+      """,
+      """
+        {
+          "separator": ","
+        }
+      """
+    )
+
+    assert(warnings.isEmpty, warnings)
+  }
+
+}
---
+++ src/test/scala/csvwcheck/normalisation/NormalisationTestUtils.scala
@@ -0,0 +1,52 @@
+package csvwcheck.normalisation
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.{NullNode, ObjectNode}
+import csvwcheck.ConfiguredObjectMapper.objectMapper
+import csvwcheck.errors.MetadataError
+import csvwcheck.normalisation.Utils.{MetadataWarnings, NormalisationContext, Normaliser}
+import sttp.client3.HttpClientSyncBackend
+
+object NormalisationTestUtils {
+  private val httpClient = HttpClientSyncBackend()
+  val starterContext = NormalisationContext(
+    node = NullNode.instance,
+    baseUrl = "https://example.com/",
+    language = "und",
+    propertyPath = Array(),
+    httpClient = httpClient
+  )
+
+  def assertObjectNormalisation(normalisers: Map[String, Normaliser], unNormalisedObjectNodeJson: String, expectedNormalisedObjectNodeJson: String): MetadataWarnings = {
+    val Right((normalisedObject, warnings)) = Utils.normaliseObjectNode(
+      normalisers,
+      starterContext.withNode(objectMapper.readTree(unNormalisedObjectNodeJson).asInstanceOf[ObjectNode])
+    )
+    val expectedNormalisedObjectNode = objectMapper.readTree(expectedNormalisedObjectNodeJson).asInstanceOf[ObjectNode]
+    assertJsonNodesEquivalent(normalisedObject, expectedNormalisedObjectNode)
+
+    warnings
+  }
+
+  def assertJsonNodesEquivalent(actual: JsonNode, expected: JsonNode) = {
+    assert(actual == expected, s"${actual.toPrettyString} != ${expected.toPrettyString}")
+  }
+
+  def assertObjectNormalisationFailure(normalisers: Map[String, Normaliser], unNormalisedObjectNodeJson: String): MetadataError = {
+    val Left(metadataError) = Utils.normaliseObjectNode(
+      normalisers,
+      starterContext.withNode(objectMapper.readTree(unNormalisedObjectNodeJson).asInstanceOf[ObjectNode])
+    )
+    metadataError
+  }
+
+  def jsonToObjectNode(nodeJson: String) =
+    objectMapper.readTree(nodeJson).asInstanceOf[ObjectNode]
+
+
+  implicit class MetadataWarningsExtensions(warnings: MetadataWarnings) {
+    def containsWarningWith(expectedMessageFragment: String): Boolean = {
+      warnings.exists(_.message.contains(expectedMessageFragment))
+    }
+  }
+}
